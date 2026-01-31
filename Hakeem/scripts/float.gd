c4f85f5d8e082206b355f858cdd67b96bb70855f
Hakeem-Taha-06 <hakeem.taha06@eng-st.cu.edu.eg>
2026-01-31 15:19:14 +0200

    add float animation to ghost

---
+++ Hakeem/scripts/float.gd
@@ -0,0 +1,16 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	float_object_up()	
+
+func float_object_up():
+	var tween = create_tween()
+	tween.tween_property(self, "position", position + Vector3(0, 0.5, 0), 1).set_trans(Tween.TRANS_SINE)
+	tween.tween_callback(float_object_down)
+	
+func float_object_down():
+	var tween = create_tween()
+	tween.tween_property(self, "position", position + Vector3(0, -0.5, 0), 1).set_trans(Tween.TRANS_SINE)
+	tween.tween_callback(float_object_up)
