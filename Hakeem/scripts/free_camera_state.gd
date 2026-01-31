6e227eb02ad3e5aa4e7077f8772b3f22cd785e8f
Mohamed Sheref <melezaly@gmail.com>
2026-01-31 23:52:13 +0200

    seperated ghosts

--- Hakeem/scripts/free_camera_state.gd
+++ Hakeem/scripts/free_camera_state.gd
@@ -1,13 +1,18 @@
 extends GameState
+@onready var ui: Node = $"../../ui"
 
 func enter():
 	if game_manager.freecam_3d:
 		game_manager.freecam_3d.make_current()
 		game_manager.freecam_3d.movement_active = true
+		for child: CanvasItem in ui.get_children():
+			child.visible = true
 		print("entered freecam state")
 	
 func exit():
 	game_manager.freecam_3d.movement_active = false
+	for child: CanvasItem in ui.get_children():
+		child.visible = false
 	
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("switch_cam"):
